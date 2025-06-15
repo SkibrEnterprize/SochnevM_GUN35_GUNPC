2c4ac99533ec28c4b54c32e587d855ec04be3a5d
SkibrEnterprize <mle.skibr@gmail.com>
2025-06-15 20:35:58 +0300

    Initial commit

--- Assets/CellPaletteSettings.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Cell Palette Settings", menuName = "Custom/Cell")]
-public class CellPaletteSettings : ScriptableObject
-{
-    [SerializeField] Material _cellIsSelect;
-    [SerializeField] Material _cellIsAccessForAction;
-}
--- Assets/Scripts/Select.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-[RequireComponent(typeof(MeshRenderer))]
-public class Select : MonoBehaviour
-{    
-}
