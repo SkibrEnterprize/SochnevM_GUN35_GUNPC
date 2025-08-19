8f68ca7bfd6b009d125da84426758be8e2648588
SkibrEnterprize <mle.skibr@gmail.com>
2025-08-19 22:28:14 +0300

    Initial commit

--- Assets/_Project/Scripts/DOTween/MovementPath.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Path/Movement Path")]
-public class MovementPath : ScriptableObject
-{
-    [field: SerializeField]
-    public Vector3[] Points { get; private set; }
-}
--- Assets/_Project/Scripts/Signals/GameEvent.cs
+++
@@ -1,2 +0,0 @@
-public struct LidarDetected{}
-public struct DirtCollected{}
