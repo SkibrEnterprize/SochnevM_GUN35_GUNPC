d521cfe15fe2cddc88d32e13e0e87f955743df97
SkibrEnterprize <mle.skibr@gmail.com>
2025-10-17 23:21:12 +0300

    Add Logic for Movement and MessagePusher

--- Assets/Scripts/Characters/WorkerMovement.cs
+++ Assets/Scripts/Characters/WorkerMovement.cs
@@ -1,12 +1,73 @@
-﻿
+﻿using System;
+using UnityEngine;
+
 namespace Netologia.Quest.Characters
 {
-	// Поведение рабочего в фоновом режиме
-	public class WorkerMovement : BaseMovement
-	{
-		protected override void Awake()
-		{
-			base.Awake();
-		}
-	}
+    // Поведение рабочего в фоновом режиме
+    public class WorkerMovement : BaseMovement
+    {
+
+        //todo переместить всю логику в кэректера
+        [Serializable]
+        public struct WorkPoint
+        {
+            public Vector3 Position;
+            public Interval StayInterval;
+        }
+
+
+        public Vector3 Position;
+        public Interval Staylnterval;
+
+        private float? _delay;
+        private int _index;
+        private Character _character;
+        [SerializeField] private WorkPoint[] _points;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _character = GetComponent<Character>();
+            if (_points.Length <= 1)
+            {
+                _agent.enabled = false; enabled = false;
+            }
+        }
+        private void Update()
+        {
+            if (!TimeManager.IsGame) return;
+            //Если бот взаимодействует с игроком - он никуда не идёт if (!_character.MoveAccess) return;
+            //Movement state if (IsMove) return;
+            var time = TimeManager.Time;
+            //Arrived state
+            if (!_delay.HasValue)
+            {
+                Debug.Log("Check: Arrived");
+                _delay = time + _points[_index].StayInterval.Random;
+            }
+            //Change point stay
+            if (_delay - time <= 0)//or -delay < time
+            {
+                _index = (_index + 1) % _points.Length; SetPosition(_points[_index].Position);
+                _delay = null;
+                Debug.Log("Check: Change point stay");
+            }
+        }
+
+#if UNITY_EDITOR
+        [ContextMenu("Set Self Position")]
+        private void SetSelfPosition()
+=> UnityEditor.ArrayUtility.Add(ref _points, new WorkPoint { Position = transform.position }); [ContextMenu("Back To First Position")]
+        private void BackToFirstPosition()
+        {
+            if (_points.Length == 0) return; transform.position = _points[0].Position;
+        }
+        [UnityEditor.CustomEditor(typeof(WorkerMovement))]
+
+        private class Editor : Quest.Editor.OldCustomInspector { }
+#endif
+    }
 }
+
+
+    
