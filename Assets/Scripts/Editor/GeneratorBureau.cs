951532301002d7bff9616d4f336bc155a2d03369
SkibrEnterprize <mle.skibr@gmail.com>
2025-11-08 21:09:30 +0300

    Add Generate method in Generator Bureau

--- Assets/Scripts/Editor/GeneratorBureau.cs
+++ Assets/Scripts/Editor/GeneratorBureau.cs
@@ -1,13 +1,82 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
 
 namespace Netologia.Quest.Editor
 {
 	public class GeneratorBureau
 	{
-		[MenuItem("Netologia/Quest/Generate Bureau", priority = 1)]
+        private const string c_path = "Scripts/Generated/InformationBureauGenerate.cs"; 
+        private const string c_messageHeader = "<b>[Bureau]</b>: <i>{0}";
+
+        [MenuItem("Netologia/Quest/Generate Bureau", priority = 1)]
 		private static void Generate()
 		{
-			//todo HomeWork
-		}
+            var mainBuilder = new StringBuilder();
+            var argBuilder = new StringBuilder();
+            var callBuilder = new StringBuilder(); var messageBuilder = new StringBuilder();
+            var bureauType = typeof(InformationBureau);
+
+            mainBuilder.AppendLine("using UnityEngine; \t");
+            mainBuilder.AppendLine($"namespace {bureauType.Namespace} \n{{");
+            mainBuilder.AppendLine("///_______________________________");
+            mainBuilder.AppendLine("/// ------AUTOGENERETED FILE------");
+            mainBuilder.AppendLine("///_______________________________");
+            mainBuilder.AppendLine("\tpublic static partial class InformationBureau\n\t{");
+
+            var events = bureauType.GetEvents(BindingFlags.Static | BindingFlags.Public);
+            foreach (var info in events)
+            {
+                var name = info.Name;
+                FindAllArgs(info, argBuilder, callBuilder, messageBuilder);
+                mainBuilder.AppendLine($"\t\tpublic static void Call{name} ({argBuilder})");
+                mainBuilder.AppendLine($"\t\t{{");
+                mainBuilder.AppendLine($"\t\t\tDebug.Log($\"{messageBuilder}\");");
+                mainBuilder.AppendLine($"\t\t\tif ({name} == null)\n\t\t\t{{");
+                mainBuilder.AppendLine($"\t\t\t\tDebug.Log(\"{string.Format(c_messageHeader, $"{name}</i> is <b>NULL</b>")}\");");
+                mainBuilder.AppendLine($"\t\t\t\treturn;\n\t\t\t}}");
+                mainBuilder.AppendLine($"\t\t\t{name}. Invoke({callBuilder});"); mainBuilder.AppendLine($"\t\t}}\n");
+            }
+
+            mainBuilder.AppendLine("\t}\n}");
+
+            var path = Path.Combine(Application.dataPath, c_path);
+            if (!File.Exists(path)) File.Create(path).Close();
+            File.WriteAllText(path, mainBuilder.ToString());
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+        private static void FindAllArgs(EventInfo info, StringBuilder args, StringBuilder calls, StringBuilder mes)
+        {
+            const string spliter = ", ";
+
+            var type = info.EventHandlerType;
+            args.Clear(); calls.Clear(); mes.Clear();
+            if (!type.IsGenericType)
+            {
+                mes.Append($" {string.Format(c_messageHeader, info.Name)}()</i>");
+                return;
+            }
+
+            mes.Append($"{string.Format(c_messageHeader, info.Name)}(");
+            var array = type.GenericTypeArguments;
+            for (int i = 0, iMax = array.Length; i < iMax; i++)
+            {
+                args.Append($" {array[i].FullName} arg{i}");
+                calls.Append($"arg{i}");
+                mes.Append($"{{arg{i}.ToString()}}");
+
+                if (i + 1 < iMax)
+                {
+                    args.Append(spliter);
+                    calls.Append(spliter);
+                    mes.Append(spliter);
+                    mes.Append("</i>");
+                }
+            }
+        }
 	}
 }
--- Assets/Scripts/Generated/InformationBureauGenerate.cs
+++ Assets/Scripts/Generated/InformationBureauGenerate.cs
@@ -1,50 +1,53 @@
-using UnityEngine;	
-namespace Netologia.Quest
+using UnityEngine; 	
+namespace Netologia.Quest 
 {
+///_______________________________
+/// ------AUTOGENERETED FILE------
+///_______________________________
 	public static partial class InformationBureau
 	{
-		public static void CallOnDialogClose()
+		public static void CallOnDialogClose ()
 		{
-			Debug.Log($"<b>[Bureau]</b>: <i>OnDialogClose()</i>");
-			if(OnDialogClose == null)
+			Debug.Log($" <b>[Bureau]</b>: <i>OnDialogClose()</i>");
+			if (OnDialogClose == null)
 			{
 				Debug.Log("<b>[Bureau]</b>: <i>OnDialogClose</i> is <b>NULL</b>");
 				return;
 			}
-			OnDialogClose.Invoke();
+			OnDialogClose. Invoke();
 		}
 
-		public static void CallOnQuestChanged(Netologia.Quest.QuestInfo arg0)
+		public static void CallOnQuestChanged ( Netologia.Quest.QuestInfo arg0)
 		{
-			Debug.Log($"<b>[Bureau]</b>: <i>OnQuestChanged({arg0.ToString()})</i>");
-			if(OnQuestChanged == null)
+			Debug.Log($"<b>[Bureau]</b>: <i>OnQuestChanged({arg0.ToString()}");
+			if (OnQuestChanged == null)
 			{
 				Debug.Log("<b>[Bureau]</b>: <i>OnQuestChanged</i> is <b>NULL</b>");
 				return;
 			}
-			OnQuestChanged.Invoke(arg0);
+			OnQuestChanged. Invoke(arg0);
 		}
 
-		public static void CallOnCharacterFocused(System.Boolean arg0, System.String arg1)
+		public static void CallOnCharacterFocused ( System.Boolean arg0,  System.String arg1)
 		{
-			Debug.Log($"<b>[Bureau]</b>: <i>OnCharacterFocused({arg0.ToString()}, {arg1.ToString()})</i>");
-			if(OnCharacterFocused == null)
+			Debug.Log($"<b>[Bureau]</b>: <i>OnCharacterFocused({arg0.ToString()}, </i>{arg1.ToString()}");
+			if (OnCharacterFocused == null)
 			{
 				Debug.Log("<b>[Bureau]</b>: <i>OnCharacterFocused</i> is <b>NULL</b>");
 				return;
 			}
-			OnCharacterFocused.Invoke(arg0, arg1);
+			OnCharacterFocused. Invoke(arg0, arg1);
 		}
 
-		public static void CallOnShowDialog(System.String arg0, System.Boolean arg1)
+		public static void CallOnShowDialog ( System.String arg0,  System.Boolean arg1)
 		{
-			Debug.Log($"<b>[Bureau]</b>: <i>OnShowDialog({arg0.ToString()}, {arg1.ToString()})</i>");
-			if(OnShowDialog == null)
+			Debug.Log($"<b>[Bureau]</b>: <i>OnShowDialog({arg0.ToString()}, </i>{arg1.ToString()}");
+			if (OnShowDialog == null)
 			{
 				Debug.Log("<b>[Bureau]</b>: <i>OnShowDialog</i> is <b>NULL</b>");
 				return;
 			}
-			OnShowDialog.Invoke(arg0, arg1);
+			OnShowDialog. Invoke(arg0, arg1);
 		}
 
 	}
